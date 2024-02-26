1fbd237e535fc048b1623526e71722ad04d60d47
dev-satyam <115274440+belief2002@users.noreply.github.com>
2024-02-26 18:16:59 +0530

    Update route.js

--- app/api/mongo/route.js
+++ app/api/mongo/route.js
@@ -4,10 +4,9 @@ const { MongoClient } = require("mongodb");
 
 export async function GET(request) {
   // Replace the uri string with your connection string.
-  const uri =
-    "mongodb+srv://satyam30patel:%40atlas%232002@mongoreaper.2suachv.mongodb.net/";
 
-  const client = new MongoClient(uri);
+
+  const client = new MongoClient(MONGODB_URI);
 
   try {
     const database = client.db("nezuko");
